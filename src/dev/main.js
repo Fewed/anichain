fa5e0371bb345c9248a0ccb8709c10ec9c677a59
Mikhail Sementsov <fewebdev@gmail.com>
2019-03-29 21:59:33 +0300

    publish

--- src/dev/main.js
+++ src/dev/main.js
@@ -1,21 +1,33 @@
-import { init, chain } from "./anichain";
+import { init, chain } from "anichain";
 
 const coords = [0, 220];
 
-const [el, el2] = init("div", coords);
+const [el] = init("div", coords);
 
 function move(dir = "x") {
 	dir === "x" ? ++this.x : ++this.y;
 }
 
-function cond(val) {
-	return el.t < val;
-}
-
 const ch = () =>
 	chain(
-		[0, el, move, () => [], () => cond(50)],
-		[1, el, move, () => ["y"], () => cond(100)],
+		[
+			0,
+			el,
+			move,
+			() => [],
+			function() {
+				return this.t < 50;
+			},
+		],
+		[
+			1,
+			el,
+			move,
+			() => ["y"],
+			function() {
+				return this.t < 100;
+			},
+		],
 		() => {
 			[el.x, el.y] = coords;
 			ch();
